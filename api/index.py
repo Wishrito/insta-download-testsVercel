101c8e978fd18dc34eaaf1658bd0ce98fd0a5311
Wishrito <contact.rayantremion@gmail.com>
2024-09-03 19:18:40 +0200

    added CSS for home page

--- api/index.py
+++ api/index.py
@@ -1,13 +1,26 @@
+from os import path
+
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-from os import path
-app = FastAPI()
 
 
-def render_page(page_name: str):
-    page = open(path.join('data', f'{page_name}.html')).read()
-    return page
+class App(FastAPI):
+    def __init__(self):
+        super().__init__()  # Appelle le constructeur de FastAPI
 
-@app.get("/", response_class=HTMLResponse)
-def read_root():
-    return render_page('home')
+        @self.get("/", response_class=HTMLResponse)
+        def read_root():
+            return self._render_page('home')
+
+    def _render_page(self, page_name: str) -> str:
+        try:
+            # Utilisation d'un contexte de gestion pour ouvrir le fichier
+            with open(path.join('data', 'src', 'pages', f'{page_name}.html'), 'r') as file:
+                page = file.read()
+            return page
+        except FileNotFoundError:
+            return "<h1>Page not found</h1>"
+
+
+# Crée une instance de l'application
+app = App()
