24224c238e5db6753ec0447f43649a60287dad21
Wishrito <contact.rayantremion@gmail.com>
2024-09-03 16:58:08 +0200

    updates

--- api/index.py
+++ api/index.py
@@ -1,10 +1,11 @@
-from sanic import Sanic
-from sanic.response import json
-
-app = Sanic(__name__)
+from http.server import BaseHTTPRequestHandler
 
 
-@app.route('/')
-@app.route('/<path:path>')
-async def index(request, path=""):
-    return json({'hello': path})
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!'.encode('utf-8'))
+        return
