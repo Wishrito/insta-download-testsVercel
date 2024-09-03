31aacacae4d221da0c5a6f511027c09e7900b0f7
Wishrito <contact.rayantremion@gmail.com>
2024-09-03 17:10:18 +0200

    new try

--- api/index.py
+++ api/index.py
@@ -1,4 +1,6 @@
+
 from http.server import BaseHTTPRequestHandler
+from os.path import join
 
 
 class handler(BaseHTTPRequestHandler):
@@ -7,5 +9,7 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+        with open(join('data', 'file.txt'), 'r') as file:
+          for line in file:
+            self.wfile.write(line.encode())
         return
