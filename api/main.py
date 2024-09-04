8a9a378fee0a266a0daa5eb81262f702ac79389b
Wishrito <contact.rayantremion@gmail.com>
2024-09-04 11:46:14 +0200

    ue

--- api/main.py
+++ api/main.py
@@ -1,11 +1,18 @@
+from os.path import join
+
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-from os.path import join
+
 app = FastAPI()
 
 
 @app.get("/", response_class=HTMLResponse)
 def read_root():
     with open(join('pages', 'home.html'), 'r') as f:
-        html_content = f.read()
+        html_content_list = f.readlines()
+    html_content = ""
+    for line in html_content_list:
+        if line.startswith('<link rel="stylesheet"'):
+            line = f"<link rel=\"stylesheet\" href=\"{join('..', 'src', 'css', 'home.css')}\">"
+            html_content += line
     return html_content
