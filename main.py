e58132ee976f87629de48d3449de8a4bb4491bda
Wishrito <contact.rayantremion@gmail.com>
2024-09-04 13:03:51 +0200

    2nd try

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from os.path import dirname, join
 from flask import Flask, render_template
 
 app = Flask(__name__)
-app.template_folder = join('..', dirname(__file__), 'pages')
-app.static_folder = join('..', dirname(__file__), 'src')
+app.template_folder = join(dirname(__file__), 'pages')
+app.static_folder = join(dirname(__file__), 'src')
 
 
 @app.route("/")
