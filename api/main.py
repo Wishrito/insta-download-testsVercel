c2027c4332d541e8c741c942c7465075030c4665
Wishrito <contact.rayantremion@gmail.com>
2024-09-04 11:27:50 +0200

    ue

--- api/main.py
+++ api/main.py
@@ -1,28 +1,11 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-
+from os.path import join
 app = FastAPI()
 
 
 @app.get("/", response_class=HTMLResponse)
 def read_root():
-    with open('../pages/home.html', 'r') as f:
+    with open(join('pages', 'home.html'), 'r') as f:
         html_content = f.read()
-    return html_content
-
-
-@app.get("/items/{item_id}", response_class=HTMLResponse)
-def read_item(item_id: int, q: str = None):
-    html_content = f"""
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Item {item_id}</title>
-    </head>
-    <body>
-        <h1>Item ID: {item_id}</h1>
-        <p>Query: {q}</p>
-    </body>
-    </html>
-    """
     return html_content
